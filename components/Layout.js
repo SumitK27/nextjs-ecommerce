2535b82ea2fe586fb203eeafb3b8a0262abee06c
SumitK27 <sumitkajbaje@gmail.com>
2021-12-11 13:51:46 +0530

    Addded Material Theme

--- components/Layout.js
+++ components/Layout.js
@@ -7,10 +7,36 @@ import {
     Link,
     Toolbar,
     Typography,
+    ThemeProvider,
+    CssBaseline,
+    createTheme,
 } from "@material-ui/core";
 import useStyles from "../utils/styles";
 
 export default function Layout({ title, description, children }) {
+    const theme = createTheme({
+        typography: {
+            h1: {
+                fontSize: "1.6rem",
+                fontWeight: 400,
+                margin: "1rem 0",
+            },
+            h2: {
+                fontSize: "1.4rem",
+                fontWeight: 400,
+                margin: "1rem 0",
+            },
+        },
+        palette: {
+            type: "light",
+            primary: {
+                main: "#f0c000",
+            },
+            secondary: {
+                main: "#208080",
+            },
+        },
+    });
     const classes = useStyles();
     return (
         <div>
@@ -20,30 +46,33 @@ export default function Layout({ title, description, children }) {
                     <meta name="description" content={description}></meta>
                 )}
             </Head>
-            <AppBar position="static" className={classes.navbar}>
-                <Toolbar>
-                    <NextLink href="/" passHref>
-                        <Link>
-                            <Typography className={classes.brand}>
-                                amazon
-                            </Typography>
-                        </Link>
-                    </NextLink>
-                    <div className={classes.grow}></div>
-                    <div>
-                        <NextLink href="/cart" passHref>
-                            <Link>Cart</Link>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <AppBar position="static" className={classes.navbar}>
+                    <Toolbar>
+                        <NextLink href="/" passHref>
+                            <Link>
+                                <Typography className={classes.brand}>
+                                    amazon
+                                </Typography>
+                            </Link>
                         </NextLink>
-                        <NextLink href="/login" passHref>
-                            <Link>Login</Link>
-                        </NextLink>
-                    </div>
-                </Toolbar>
-            </AppBar>
-            <Container className={classes.main}>{children}</Container>
-            <footer className={classes.footer}>
-                <Typography>All rights reserved. Niko.</Typography>
-            </footer>
+                        <div className={classes.grow}></div>
+                        <div>
+                            <NextLink href="/cart" passHref>
+                                <Link>Cart</Link>
+                            </NextLink>
+                            <NextLink href="/login" passHref>
+                                <Link>Login</Link>
+                            </NextLink>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <Container className={classes.main}>{children}</Container>
+                <footer className={classes.footer}>
+                    <Typography>All rights reserved. Niko.</Typography>
+                </footer>
+            </ThemeProvider>
         </div>
     );
 }
