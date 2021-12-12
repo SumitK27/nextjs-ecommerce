2dc288adca44bdbb841449882d4cc295966d05c9
SumitK27 <sumitkajbaje@gmail.com>
2021-12-12 17:11:16 +0530

    Added User Authentication

---
+++ pages/api/users/login.js
@@ -0,0 +1,37 @@
+import bcrypt from "bcryptjs";
+import nc from "next-connect";
+import User from "../../../models/User";
+import { signToken } from "../../../utils/auth";
+import db from "../../../utils/db";
+
+const handler = nc();
+
+handler.post(async (req, res) => {
+    // Connect to Database
+    await db.connect();
+
+    // Get the user from Database
+    const user = await User.findOne({ email: req.body.email });
+
+    // Disconnect from the Database
+    db.disconnect();
+
+    // Check for User and check for the password
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+        // Create token
+        const token = signToken(user);
+
+        // Send user Information back
+        res.send({
+            token,
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401).send({ message: "Invalid user or password" });
+    }
+});
+
+export default handler;
--- pages/login.js
+++ pages/login.js
@@ -6,18 +6,33 @@ import {
     TextField,
     Typography,
 } from "@material-ui/core";
+import axios from "axios";
 import NextLink from "next/link";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import useStyles from "../utils/styles";
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const classes = useStyles();
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await axios.post("/api/users/login", {
+                email,
+                password,
+            });
+            alert("Successfully Logged in");
+        } catch (err) {
+            alert(err.response.data ? err.response.data.message : err.message);
+        }
+    };
     return (
         <div>
             <Layout title="Login - Amazon">
-                <form className={classes.form}>
+                <form onSubmit={submitHandler} className={classes.form}>
                     <Typography component="h1" variant="h1">
                         Login
                     </Typography>
@@ -29,6 +44,7 @@ export default function Login() {
                                 id="email"
                                 label="Email"
                                 inputProps={{ type: "email" }}
+                                onChange={(e) => setEmail(e.target.value)}
                             ></TextField>
                         </ListItem>
                         <ListItem>
@@ -38,6 +54,7 @@ export default function Login() {
                                 id="password"
                                 label="Password"
                                 inputProps={{ type: "password" }}
+                                onChange={(e) => setPassword(e.target.value)}
                             ></TextField>
                         </ListItem>
                         <ListItem>
